712debb9a4865408072ba5e23548edba6ccbf59a
Rambod Goshtasbi <rpg225@gmail.com>
2025-01-04 13:03:11 -0500

    install actix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_files::Files;
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
+    
+    HttpServer::new(|| {
+        App::new()
+            .service(Files::new("/", "./public").index_file("index.html"))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
