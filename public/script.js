dc2c09f8d1939c2ad7ec0e927a9bb4be579aa06a
Rambod Goshtasbi <rpg225@gmail.com>
2025-01-04 12:37:48 -0500

    Setting up the starter files

---
+++ public/script.js
@@ -0,0 +1,13 @@
+let last_update = 0;
+
+let last_delta = {};
+
+// create WebSocket instance/connection
+
+
+// Handle WebSocket events
+
+
+// Create QuillJS instance
+
+// Set handling functions for the text-change event (QuillJS)
